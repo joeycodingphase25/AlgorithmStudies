3979bddb77b9f4061e9e0d41f87b42f5b3e4bea7
Joseph Madenjian <joseph.madenjian@gmail.com>
2022-06-20 13:09:18 -0400

    fourSum/kSum

--- test.py
+++ test.py
@@ -1,3 +1,15 @@
-a = {5:10}
-
-print(a.get(5))
+coins = [1,2,5]
+def fewest(coins, amount):
+    test = sorted(coins)
+    i = len(coins)-1
+    value = 0
+    count = 0
+    while i >= 0:
+        while (value+coins[i]) <= amount:
+            value += coins[i]
+            count +=1
+            if value == amount:
+                return count
+        i -= 1
+    return False
+print(fewest(coins, 11))

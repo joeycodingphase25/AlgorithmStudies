40f606c55d8f625fda1225ddb69cb3e567c3f5ca
Joseph Madenjian <joseph.madenjian@gmail.com>
2022-06-20 18:44:49 -0400

    fixed subArraySort, more inclusive

--- python/arrays/subArraySort.py
+++ python/arrays/subArraySort.py
@@ -1,8 +1,7 @@
 # Difficulty: Hard
 # O(nlogn) time, 0(n) space
 
-## This will only work if the array > 2 AND the array has two points of failure
-# need optimization improvements 
+## FIXED
 def subarraySort(array):
     compare = sorted(array)
     res = []
@@ -10,18 +9,21 @@ def subarraySort(array):
     rightHolder = False
     left = 0
     right = len(array)-1
-    while True:
-        while leftHolder == False: 
-            if array[left] != compare[left]:
-                leftHolder = True
-                res.append(left)
+    
+    while left < right and leftHolder == False: 
+        if array[left] != compare[left]:
+            leftHolder = True
+            res.append(left)
+        else:
             left += 1
-        while rightHolder == False:
-            if array[right] != compare[right]:     
-                rightHolder = True
-                res.append(right)
+    while left < right and rightHolder == False:
+        if array[right] != compare[right]:     
+            rightHolder = True
+            res.append(right)
+        else:
             right -= 1
-        break
+    if len(res) == 0:
+        return [-1,-1]
     return res
 
 # Option 2
